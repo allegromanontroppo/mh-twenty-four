51bff8d36ebef371098353b3715417a7cd88b023
Mark Holland <mark.holland@mcmssolutions.co.uk>
2022-06-18 14:48:50 +0100

    Initial Client App Install

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+    return (
+        <div className="App">
+            <header className="bg-cyan-50 px-2 py-4 mb-2 border-cyan-900">
+                <div className="container mx-auto">
+                    <p className="text-4xl font-extrabold">
+                        <span className="text-cyan-900">MH</span>
+                        <span className="text-cyan-300">:</span>
+                        <span className="text-cyan-900">24</span>
+                    </p>
+                </div>
+            </header>
+
+            <main className="container mx-auto space-y-4 px-2 pb-8"></main>
+        </div>
+    );
+}
+
+export default App;
